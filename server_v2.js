8ca05a4391a8d293691ae2c356ed93f422715d76
MulongXie <dsh15325@163.com>
2020-07-09 09:21:08 +1000

    update selective watching mode in the server

--- server_v2.js
+++ server_v2.js
@@ -41,8 +41,12 @@ app.post('/process', function (req, res) {
             if (err == null){
                 console.log('Upload image to', upload_path);
                 // processing
-                // element_detection(res, upload_path, output_path, method, uied_params)
-                element_detection_watching(res, upload_path, output_path, method, uied_params)
+                if (method == 'uied'){
+                    element_detection_watching(res, upload_path, output_path, method, uied_params)
+                }
+                else {
+                    element_detection(res, upload_path, output_path, method, uied_params)
+                }
             }
             else {
                 index --;
@@ -61,8 +65,12 @@ app.post('/process', function (req, res) {
         if (input_path_split[0] == 'http:'){
             input_path = 'public/images/screen/' + name + '.jpg';
         }
-        // element_detection(res, input_path, output_path, method, uied_params)
-        element_detection_watching(res, input_path, output_path, method, uied_params)
+        if (method == 'uied'){
+            element_detection_watching(res, input_path, output_path, method, uied_params)
+        }
+        else{
+            element_detection(res, input_path, output_path, method, uied_params)
+        }
     }
 });
 
